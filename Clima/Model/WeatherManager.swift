be48cf2e9be470216f5ed83baa7a333dda620d4c
Cassandra Mathison <cassiemathison@yahoo.com>
2023-11-30 23:06:41 -0800

    Parse JSON data to print out different attributes to console

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -13,6 +13,46 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create a URL
+        
+        if let url = URL(string: urlString) {
+            
+            
+            //2. Create a URLSession
+            
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    
+                     if let safeData = data {
+                         self.parseJSON(weatherData: safeData)
+                     }
+                }
+            
+            
+            //4. Start the task
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
     }
 }
