c0cd4b6fc6cec16b0d3947838fed52a69d27976d
Cassandra Mathison <cassiemathison@yahoo.com>
2023-12-12 21:50:51 -0800

    Create WeatherModelDelegate so features can be reused in other classes.

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -8,8 +8,14 @@
 
 import Foundation
 
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(weather: WeatherModel)
+}
+
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=72ffbc44111c8ae4f33a819b28dc5d47&units=metric"
+    
+    var delegate: WeatherManagerDelegate?
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -35,7 +41,9 @@ struct WeatherManager {
                     }
                     
                      if let safeData = data {
-                         self.parseJSON(weatherData: safeData)
+                         if let weather = self.parseJSON(weatherData: safeData) {
+                             self.delegate?.didUpdateWeather(weather: weather)
+                         }
                      }
                 }
             
@@ -46,7 +54,7 @@ struct WeatherManager {
         }
     }
     
-    func parseJSON(weatherData: Data) {
+    func parseJSON(weatherData: Data) -> WeatherModel? {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
@@ -55,9 +63,11 @@ struct WeatherManager {
             let name = decodedData.name
             
             let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
-            print(weather.temperatureString)
+            return weather
+           
         } catch {
             print(error)
+            return nil
         }
     }
     
